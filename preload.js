05d0fe573baa29d4318f9c06c3f3c906d916597b
sheep4mil-ui <sheep4mil@gmail.com>
2026-08-23 21:58:23 -0500

    Expose Electron API methods in preload script

--- preload.js
+++ preload.js
@@ -1,1 +1,7 @@
+const { contextBridge, ipcRenderer } = require('electron');
 
+contextBridge.exposeInMainWorld('electronAPI', {
+  loadExtensions: () => ipcRenderer.invoke('load-extensions'),
+  saveProjectState: (snapshot) => ipcRenderer.invoke('save-project-state', snapshot),
+  loadProjectState: () => ipcRenderer.invoke('load-project-state')
+});
